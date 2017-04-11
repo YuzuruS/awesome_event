06efa4d33049a443c9296eb491eb33968240806a
YuzuruS <navitima@gmail.com>
2017-04-12 00:27:34 +0900

    factory_girlテスト

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,43 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Event, :type => :model do
-  describe '#name' do
-    context 'nilのとき' do
-      let(:event) {Event.new(name: nil)}
-      it 'valid でないこと' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
+  describe '#created_by?' do
+    let(:event) { create(:event)}
+    subject { event.created_by?(user)}
+
+    context '引数が nil なとき' do
+      let(:user) { nil }
+      it { should be_falsey }
     end
-    context '空白のとき' do
-      let(:event) {Event.new(name: '')}
-      it 'valid でないこと' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
+
+    context '#owner_id と 引数の#id が同じ時' do
+      let(:user) { double('user', id:event.id)}
+      it { should be_truthy}
     end
-    context 'Rails勉強会のとき' do
-      let(:event) {Event.new(name: 'Rails勉強会')}
-      it 'valid であること' do
-        event.valid?
-        expect(event.errors[:name]).to be_blank
-      end
-    end
-    context '50文字のとき' do
-      let(:event) {Event.new(name: 'a' * 50)}
-      it 'valid であること' do
-        event.valid?
-        expect(event.errors[:name]).to be_blank
-      end
-    end
-    context '51文字のとき' do
-      let(:event) {Event.new(name: 'a' * 51)}
-      it 'valid でないこと' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
-    # it { should validate_presence_of(:name)}
-    # it { should ensure_length_of(:name).is_at_most(50)}
   end
 end
